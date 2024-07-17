a0ed39963ab02219eb687205597252618c7e16d1
karrie2000 <gatlinkarrington21@gmail.com>
2024-07-17 12:20:52 -0500

    first commit

---
+++ network/network-script.js
@@ -0,0 +1,11 @@
+// const likeButton = document.querySelector("likeButton");
+// const likes = document.getElementById("like-count");
+
+function incrementLikes(id) {
+  const likes = document.getElementById(id);
+  likes.textContent++;
+}
+
+// likeButton.addEventListener("click", function () {
+//   incrementLikes(likes);
+// });
